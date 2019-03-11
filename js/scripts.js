8b8e5b4dc78cfafef374cc9b6fd29124021a6a62
Akigawa <sonoskeakigawa@gmail.com>
2019-03-11 15:42:28 -0700

    committing to bullying brian

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,41 @@
+var taskBook = new Taskbook();
 $(document).ready(function() {
-//var taskBook = new Taskbook();
-$("#taskForm").submit(function(event){
-event.preventDefault();
-newTask = new Task($("#taskname").val(), $("#taskdesc").val(), $("#prereqs").val(), false );
-addtask(newTask);
-console.log(newTask);
+  //var taskBook = new Taskbook();
+  var newTask = new Task;
+  $("#taskForm").submit(function(event){
+    event.preventDefault();
+    newTask = new Task($("#taskname").val(), $("#taskdesc").val(), $("#prereqs").val(), false );
+    console.log(newTask);
+    taskBook.addtask(newTask);
+    $("#tasks").html("");
+
+    Refreshtasks(taskBook);
+
+  });
+  // Stuff to do as soon as the DOM is ready
 });
+function Refreshtasks(book)
+{
+  $("#tasks").html("");
+  book.tasks.forEach(function(tsk){
+    $("#tasks").append(tsk.taskPrint());
+    Attachremove(tsk);
+  });
+  console.log("Done refreshing.");
+}
 
+function Attachremove(tsk)
+{
+  console.log("#task" + tsk.id + "close");
+  $("#task" + tsk.id + "close").click(function(){
+    taskBook.tasks.splice(window[tsk.id] - 1, 1);
+    Refreshtasks(taskBook);
+    console.log("Done adding close button");
+      console.log("#task" + tsk.id + "close");
+  });
+  console.log("Done adding close button");
+}
 
-
-   // Stuff to do as soon as the DOM is ready
-
-});
 
 Taskbook.prototype.addtask = function(task) {
   task.id = this.assignId();
@@ -35,23 +59,24 @@ Taskbook.prototype.findtask = function(id) {
 }
 
 function Task(taskname, taskbody, requirements, completed) {
-this.taskname = taskname,
-this.taskbody = taskbody,
-this.requirements = requirements,
-this.completed = completed
+  this.taskname = taskname,
+  this.taskbody = taskbody,
+  this.requirements = requirements,
+  this.completed = completed
 }
-function Taskbook()
-{
-this.contacts = [],
-this.currentId = 0
+
+function Taskbook(){
+  this.tasks = [],
+  this.currentId = 0
 
 }
-task.prototype.taskPrint = function()
-{
-this = "";
-this += "<div><div><br>" taskname + "<br></div>";
-this += "<div>" + taskdesc + "</div>";
-this += "<input type='checkbox'>" + "</div>";
-this += "</div>";
 
+Task.prototype.taskPrint = function(){
+  var taskprint= "<div id='" + "task" + this.id + "'>";
+  taskprint += "<div class='container main'><div class=''> Name:<br>" + this.taskname + "<br></div>";
+  taskprint += "<div> Description:" + this.taskbody + "</div>";
+  taskprint += " Completed? <input type='checkbox' id='" + "task" + this.id + "completed'></input>";
+  taskprint += "<br><input type='button' value='close' id='task" + this.id + "close'></input>  " + "</div>";
+  taskprint += "</div></div>";
+  return taskprint;
 }
